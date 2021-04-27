98b1d15b8879cabf6cb06d335acf900395a7cc9c
Gustavo Garcia <misiotven@gmail.com>
2021-04-27 00:03:24 -0400

    Casi Completado

---
+++ pages/templates.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+
+    
+
+  </div>
+</template>
+
+<script>
+import Iotbutton from '~/components/Widgets/Iotbutton.vue';
+
+export default {
+  components: { Iotbutton },
+  data() {
+    return {
+      value: false,
+
+      configButton: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
+        },
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "danger",
+        message: "{'fanstatus': 'stop'}"
+      },
+
+
+
+      configIndicator: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
+        },
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "success"
+      },
+    };
+  },
+  methods: {
+    
+  }
+}
+</script>
